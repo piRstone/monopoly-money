ca5f224c0ef0561c0acd29698856f87b193410bc
piRstone <pierre.lavalley@gmail.com>
2016-03-12 22:06:43 +0100

    reset game commande

--- api/postResetGame.php
+++ api/postResetGame.php
@@ -3,6 +3,6 @@
 $bdd = new PDO('mysql:host=localhost;dbname=monopoly;charset=utf8', 'root', '');
 
 $bdd->exec("UPDATE properties SET owner_id = NULL, nbHouses = 0 WHERE id > 0");
-$bdd->exec("UPDATE users SET credit = 1490 WHERE id > 0");
+$bdd->exec("UPDATE users SET credit = 1500, nbGares = 0, nbCompagnies = 0 WHERE id > 0");
 
 ?>
